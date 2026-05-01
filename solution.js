b1c210cc645749f2c2f0000253f0456cc199d172
Alijah LeMay <36865734+Alijah-LeMay@users.noreply.github.com>
2026-04-30 21:31:20 -0700

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,11 @@
+function solution(arrayA, arrayB) {
+  // TODO: Implement the function
+  
+  for(let i=0;i<arrayA.length;i++) {
+    for(let j=0;i<arrayB.length;i++) {
+      
+    }
+  }
+}
+
+module.exports = { solution };
